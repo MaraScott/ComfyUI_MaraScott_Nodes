09c15fb3b8023bac65d4f7d5c119882223cf7724
davask <contact@davask.com>
2024-02-03 22:33:02 +0100

    updae version to 1.1.0

--- Marasit_Nodes.py
+++ Marasit_Nodes.py
@@ -3,21 +3,9 @@
 #
 ###
 #
-# File: __init__.py
-# Project: ComfyUI-MarasIT-Nodes
-# By MarasIT (Discord: davask#4370)
-# Copyright 2024 David Asquiedge (davask)
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to
-# deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+###
 
 from .py.inc import web
 
@@ -27,7 +15,7 @@ from .py.nodes.bus import Marasit_Bus
 
 MANIFEST = {
     "name": "Maras IT Nodes",
-    "version": (1,0,0),
+    "version": (1,1,0),
     "author": "davask",
     "project": "https://github.com/davask/ComfyUI-MarasIT-Nodes",
     "description": "A simple Bus node",
