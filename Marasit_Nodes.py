c79dfe2245e0d332facc0319ee297fa21967ab59
davask <contact@davask.com>
2024-02-15 17:35:50 +0100

    hotfix empty node on creation

--- Marasit_Nodes.py
+++ Marasit_Nodes.py
@@ -37,17 +37,19 @@ if hasattr(PromptServer, "instance"):
         nid = json_data.get("node_id")
 
         filename = f"session_{sid}_node_{nid}.json"
-        print(filename)
         filepath = os.path.join(__SESSIONS_DIR__, filename)
         # Write the data to the file
         with open(filepath, 'w') as file:
             json.dump(profile, file)
 
-        filename = f"profile_{profile}.json"
-        filepath = os.path.join(__PROFILES_DIR__, filename)
-        # Write the data to the file
-        with open(filepath, 'w') as file:
-            json.dump(inputs, file)
+        print(len(inputs))
+        print(len(inputs) > 0)
+        if len(inputs) > 0:
+            filename = f"profile_{profile}.json"
+            filepath = os.path.join(__PROFILES_DIR__, filename)
+            # Write the data to the file
+            with open(filepath, 'w') as file:
+                json.dump(inputs, file)
 
         return web.json_response(
             {"message": f"profile: {profile} | id: {nid}"}
