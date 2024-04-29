54ce2e567b6fe84a1f51497b5c2c81cb93498802
davask <contact@davask.com>
2024-04-29 23:59:42 +0200

    Add Sigmas_type management

--- py/nodes/UpscalerRefinerNode.py
+++ py/nodes/UpscalerRefinerNode.py
@@ -20,13 +20,26 @@ from ..utils.log import *
 
 class UpscalerRefinerNode:
     
+    sigmas_type = None
+    model = None
+    scheduler = None
+    steps = None
+    denoise = None
+    
+    SIGMAS_TYPES = [
+        "BasicScheduler"
+        , "SDTurboScheduler"
+        # , "AlignYourStepsScheduler"
+    ]
+    
     @classmethod
     def INPUT_TYPES(self):
         return {
             "hidden": {"id":"UNIQUE_ID"},
             "required":{
                 "image": ("IMAGE",),
-                "model_name": (folder_paths.get_filename_list("upscale_models"),),
+
+                "upscale_model": (folder_paths.get_filename_list("upscale_models"),),
                 
                 "feather_mask": ("INT", {"default": 350, "min": 0, "max": nodes.MAX_RESOLUTION, "step": 1}),                
 
@@ -37,12 +50,13 @@ class UpscalerRefinerNode:
                 "seed": ("INT", {"default": 4, "min": 0, "max": 0xffffffffffffffff}),
                 "steps": ("INT", {"default": 10, "min": 1, "max": 10000}),
                 "cfg": ("FLOAT", {"default": 2.5, "min": 0.0, "max": 100.0, "step":0.1, "round": 0.01}),
+
+                "sigmas_type": (self.SIGMAS_TYPES, ),
                 "sampler_name": (comfy.samplers.KSampler.SAMPLERS, ),
-                "scheduler": (comfy.samplers.KSampler.SCHEDULERS, ),
+                "basic_scheduler": (comfy.samplers.KSampler.SCHEDULERS, ),
 
                 "positive": ("CONDITIONING", ),
                 "negative": ("CONDITIONING", ),
-                # "latent_image": ("LATENT", ),
 
                 "denoise": ("FLOAT", {"default": 0.35, "min": 0.0, "max": 0.51, "step": 0.01}),                
 
@@ -54,18 +68,12 @@ class UpscalerRefinerNode:
     RETURN_TYPES = (
         "IMAGE", 
         "IMAGE", 
-        "IMAGE", 
-        "IMAGE", 
-        # "MASK", 
         "STRING"
     )
     
     RETURN_NAMES = (
-        "final_image", 
-        "refined_image", 
-        "upscaled_image", 
-        "resized_image", 
-        # "mask", 
+        "image", 
+        "original_resized", 
         "info", 
     )
     
@@ -74,32 +82,47 @@ class UpscalerRefinerNode:
     DESCRIPTION = "An \"UPSCALER\" Node"
     FUNCTION = "fn"
     
+    @classmethod    
+    def __get_sigmas(self):        
+        if self.sigmas_type == "SDTurboScheduler":
+            SigmaScheduler = getattr(comfy_extras.nodes_custom_sampler, self.sigmas_type)
+            sigmas = SigmaScheduler.get_sigmas(SigmaScheduler, self.model, self.steps, self.denoise)[0]
+        else: # BasicScheduler
+            SigmaScheduler = getattr(comfy_extras.nodes_custom_sampler, self.sigmas_type)
+            sigmas = SigmaScheduler.get_sigmas(SigmaScheduler, self.model, self.scheduler, self.steps, self.denoise)[0]
+
+        return sigmas
+            
+    @classmethod    
     def fn(self, **kwargs):
-        
+                
         # Initialize the bus tuple with None values for each parameter
         image = kwargs.get('image', None)
-        model_name = kwargs.get('model_name', None)
+        upscale_model_name = kwargs.get('upscale_model', None)
+        upscale_model = comfy_extras.nodes_upscale_model.UpscaleModelLoader.load_model(comfy_extras.nodes_upscale_model.UpscaleModelLoader, upscale_model_name)[0]
         feather_mask = kwargs.get('feather_mask', None)
-        upscale_model = comfy_extras.nodes_upscale_model.UpscaleModelLoader.load_model(comfy_extras.nodes_upscale_model.UpscaleModelLoader, model_name)[0]
         vae = kwargs.get('vae', None)
         tile_size = kwargs.get('tile_size', None)
-        model = kwargs.get('model', None)
-        seed = kwargs.get('seed', None)
-        steps = kwargs.get('steps', None)
+        self.model = model = kwargs.get('model', None)
+        noise_seed = kwargs.get('seed', None)
+        self.steps = kwargs.get('steps', None)
         cfg = kwargs.get('cfg', None)
         sampler_name = kwargs.get('sampler_name', None)
-        scheduler = kwargs.get('scheduler', None)
+        sampler = comfy_extras.nodes_custom_sampler.KSamplerSelect.get_sampler(comfy_extras.nodes_custom_sampler.KSamplerSelect,sampler_name)[0]
+        self.scheduler = kwargs.get('basic_scheduler', None)
         positive = kwargs.get('positive', None)
         negative = kwargs.get('negative', None)
-        denoise = kwargs.get('denoise', None)
-        
+        add_noise = True        
+        self.denoise = kwargs.get('denoise', None)        
+        self.sigmas_type = kwargs.get('sigmas_type', None)
+        sigmas = self.__get_sigmas()
         output_info = [f"No info"]
         
         if image is None:
-            raise ValueError("UpscalerGridNode id XX: No image provided")
+            raise ValueError("MarasitUpscalerRefinerNode id XX: No image provided")
 
         if not isinstance(image, torch.Tensor):
-            raise ValueError("UpscalerGridNode id XX: Image provided is not a Tensor")
+            raise ValueError("MarasitUpscalerRefinerNode id XX: Image provided is not a Tensor")
         
         image_width = image.shape[2]
         image_height = image.shape[1]
@@ -116,65 +139,49 @@ class UpscalerRefinerNode:
 
         output_images = []
         for grid_image in grid_images:            
-            # Encode the upscaled image using the VAE 
             _image_grid = grid_image[:,:,:,:3]
             upscaled_image_grid = comfy_extras.nodes_upscale_model.ImageUpscaleWithModel.upscale(comfy_extras.nodes_upscale_model.ImageUpscaleWithModel, upscale_model, _image_grid)[0]
 
             latent_image = nodes.VAEEncodeTiled.encode(nodes.VAEEncodeTiled, vae, upscaled_image_grid, tile_size)[0]
             
-            # Use the latent image in the common_ksampler function
-            # latent_output = comfy_extras.nodes_custom_sampler.SamplerCustom.sample(
-            #     comfy_extras.nodes_custom_sampler.SamplerCustom, 
-            #     model, 
-            #     add_noise, 
-            #     noise_seed, 
-            #     cfg, 
-            #     positive, 
-            #     negative, 
-            #     sampler, 
-            #     sigmas, 
-            #     latent_image
-            # )
-            latent_output = nodes.KSampler.sample(
-                nodes.KSampler,
+            latent_output = comfy_extras.nodes_custom_sampler.SamplerCustom.sample(
+                comfy_extras.nodes_custom_sampler.SamplerCustom, 
                 model, 
-                seed, 
-                steps, 
+                add_noise, 
+                noise_seed, 
                 cfg, 
-                sampler_name, 
-                scheduler, 
                 positive, 
                 negative, 
-                latent_image, 
-                denoise
+                sampler, 
+                sigmas, 
+                latent_image
             )[0]
             output = nodes.VAEDecodeTiled.decode(nodes.VAEDecodeTiled, vae, latent_output, tile_size)[0].unsqueeze(0)
             
-            # Collect all outputs (you may want to adjust this depending on how you want to handle the outputs)
             output_images.append(output[0])
 
-        refined_image = Image.rebuild_image_from_parts(output_images, upscaled_image, feather_mask)
+        output_image = Image.rebuild_image_from_parts(output_images, upscaled_image, feather_mask)
 
-        latent_image = nodes.VAEEncodeTiled.encode(nodes.VAEEncodeTiled, vae, refined_image, tile_size)[0]
+        # latent_image = nodes.VAEEncodeTiled.encode(nodes.VAEEncodeTiled, vae, refined_image, tile_size)[0]
 
-        steps = 10
-        cfg = 2.5
-        denoise = 0.1
-        # Use the latent image in the common_ksampler function
-        latent_output = nodes.KSampler.sample(
-            nodes.KSampler,
-            model, 
-            seed, 
-            steps, 
-            cfg, 
-            sampler_name, 
-            scheduler, 
-            positive, 
-            negative, 
-            latent_image, 
-            denoise
-        )[0]
-        output_image = nodes.VAEDecodeTiled.decode(nodes.VAEDecodeTiled, vae, latent_output, tile_size)[0]
+        # steps = 10
+        # cfg = 2.5
+        # denoise = 0.1
+        # sigmas = comfy_extras.nodes_custom_sampler.BasicScheduler.get_sigmas(comfy_extras.nodes_custom_sampler.BasicScheduler, model, scheduler, steps, denoise)[0]
+        # latent_output = comfy_extras.nodes_custom_sampler.SamplerCustom.sample(
+        #     comfy_extras.nodes_custom_sampler.SamplerCustom, 
+        #     model, 
+        #     add_noise, 
+        #     noise_seed, 
+        #     cfg, 
+        #     positive, 
+        #     negative, 
+        #     sampler, 
+        #     sigmas, 
+        #     latent_image
+        # )[0]
+        
+        # output_image = nodes.VAEDecodeTiled.decode(nodes.VAEDecodeTiled, vae, latent_output, tile_size)[0]
                 
         output_image_width = output_image.shape[2]
         output_image_height = output_image.shape[1]
@@ -196,9 +203,6 @@ IMAGE (OUTPUT)
         
         return (
             output_image,
-            refined_image,
-            upscaled_image,
             resized_image,
-            # output_mask,
             output_info
         )
