c4230065daf38fc25be7b5b802f96cf1d547adcc
MaraScott <david.asquiedge@marascott.ai>
2024-06-15 03:42:41 +0200

    hotfix prestartup_script

--- prestartup_script.py
+++ prestartup_script.py
@@ -12,4 +12,3 @@ from custom_nodes.ComfyUI_MaraScott_Nodes.py.inc.lib.llm import MS_Llm
 print('\033[34m[MaraScott] \033[92mInitialization\033[0m')
 
 MS_Llm.prestartup_script()
-print('\033[34m[MaraScott] \033[92mInitialization\033[0m')
