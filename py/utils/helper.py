d1fe0d717b78ee3b75b0222e4d4b31280371916a
MaraScott <david.asquiedge@marascott.ai>
2024-09-14 15:12:26 +0200

    add MaraScott/Loop, inspired by Easy Loop

--- py/utils/helper.py
+++ py/utils/helper.py
@@ -2,3 +2,23 @@
 # -*- coding:utf-8 -*-
 #
 ###
+
+class AlwaysEqualProxy(str):
+    def __eq__(self, _):
+        return True
+
+    def __ne__(self, _):
+        return False
+
+class TautologyStr(str):
+    def __ne__(self, other):
+        return False
+
+class ByPassTypeTuple(tuple):
+    def __getitem__(self, index):
+        if index>0:
+            index=0
+        item = super().__getitem__(index)
+        if isinstance(item, str):
+            return TautologyStr(item)
+        return item
