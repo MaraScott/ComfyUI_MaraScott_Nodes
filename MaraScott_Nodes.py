ad09d4fba3209175fc2ee166d235abb5c2db04eb
MaraScott <david.asquiedge@marascott.ai>
2024-09-18 16:26:52 +0200

    isEqualNode added

--- MaraScott_Nodes.py
+++ MaraScott_Nodes.py
@@ -16,7 +16,7 @@ from .py.nodes.UpscalerRefiner.McBoaty_v5 import McBoaty_UpscalerRefiner_v5, McB
 from .py.nodes.KSampler.InpaintingTileByMask_v1 import KSampler_setInpaintingTileByMask_v1, KSampler_pasteInpaintingTileByMask_v1
 from .py.nodes.Prompt.PromptFromImage_v1 import PromptFromImage_v1
 from .py.nodes.Loop.ForLoop_v1 import ForLoopOpen_v1, ForLoopClose_v1, ForLoopWhileOpen_v1, ForLoopWhileClose_v1, ForLoopIntMathOperation_v1, ForLoopToBoolNode_v1
-from .py.nodes.Util.Conditional import IsEmpty_v1, IsNone_v1, IsEmptyOrNone_v1
+from .py.nodes.Util.Conditional import IsEmpty_v1, IsNone_v1, IsEmptyOrNone_v1, IsEqual_v1
 from .py.vendor.ComfyUI_JNodes.blob.main.py.prompting_nodes import TokenCounter as TokenCounter_v1
 
 WEB_DIRECTORY = "./web/assets/js"
@@ -43,9 +43,10 @@ NODE_CLASS_MAPPINGS = {
     "MaraScottForLoopIntMathOperation_v1": ForLoopIntMathOperation_v1,
     "MaraScottForLoopToBoolNode_v1": ForLoopToBoolNode_v1,
     
-    "MaraScottIsEmpty": IsEmpty_v1,
-    "MaraScottIsNone": IsNone_v1,
-    "MaraScottIsEmptyOrNone": IsEmptyOrNone_v1,
+    "MaraScottIsEmpty_v1": IsEmpty_v1,
+    "MaraScottIsNone_v1": IsNone_v1,
+    "MaraScottIsEmptyOrNone_v1": IsEmptyOrNone_v1,
+    "MaraScottIsEqual_v1": IsEqual_v1,
 
     "MaraScottPromptFromImage_v1": PromptFromImage_v1,
     "MaraScottDisplayInfo_v1": DisplayInfoNode_v1,
@@ -80,9 +81,10 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "MaraScottForLoopIntMathOperation_v1": "\ud83d\udc30 For Loop IntMathOperation v1 /l",
     "MaraScottForLoopToBoolNode_v1": "\ud83d\udc30 For Loop ToBoolNode v1 /l",
 
-    "MaraScottIsEmpty": "\ud83d\udc30 Is Empty v1 /c",
-    "MaraScottIsNone": "\ud83d\udc30 Is None v1 /c",
-    "MaraScottIsEmptyOrNone": "\ud83d\udc30 Is Empty Or None v1 /c",
+    "MaraScottIsEmpty_v1": "\ud83d\udc30 Is Empty v1 /c",
+    "MaraScottIsNone_v1": "\ud83d\udc30 Is None v1 /c",
+    "MaraScottIsEmptyOrNone_v1": "\ud83d\udc30 Is Empty Or None v1 /c",
+    "MaraScottIsEqual_v1": "\ud83d\udc30 Is Equal v1 /c",
 
     "MaraScottPromptFromImage_v1": "\ud83d\udc30 Prompt From Image - McPrompty v1 /p",
     "MaraScottAnyBus_v2": "\ud83d\udc30 AnyBus - UniversalBus v2 /*",
