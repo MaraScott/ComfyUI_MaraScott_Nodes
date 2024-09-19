e4f82cd4e187e6995a7c588e7e65e8fcd6a74e8f
MaraScott <david.asquiedge@marascott.ai>
2024-09-19 17:17:41 +0200

    improve render of display info

--- py/nodes/Info/DisplayInfo_v2.py
+++ py/nodes/Info/DisplayInfo_v2.py
@@ -15,10 +15,20 @@ from ...utils.log import log
 
 any_type = AnyType("*")
 
+# Attempt to import numpy and torch, handle if they are not available
+try:
+    import numpy as np
+except ImportError:
+    np = None
+
+try:
+    import torch
+except ImportError:
+    torch = None
+
 class DisplayInfo_v2:
-    
     NAME = get_name('Display Any')
-    
+
     @classmethod
     def INPUT_TYPES(s):
         return {
@@ -41,15 +51,34 @@ class DisplayInfo_v2:
     CATEGORY = get_category('utils')
 
     def fn(self, any=None, unique_id=None, extra_pnginfo=None):
+        log(any)
+        value = self.serialize_any(any)
+        return {"ui": {"text": (value,)}, "result": (any, )}
 
-        value = 'None'        
-        if any is not None:
+    def serialize_any(self, any):
+        if any is None:
+            return 'None'
+        elif isinstance(any, (str, int, float, bool)):
+            return str(any)
+        elif isinstance(any, (list, tuple)):
             try:
-                value = json.dumps(any)
+                return json.dumps(any)
             except Exception:
-                try:
-                    value = str(any)
-                except Exception:
-                    value = 'any exists, but could not be serialized.'
-                    
-        return {"ui": {"text": (value,)}, "result": (value,)}
+                return str(any)
+        elif isinstance(any, dict):
+            try:
+                return json.dumps(any)
+            except Exception:
+                return str(any)
+        elif np is not None and isinstance(any, np.ndarray):
+            return f'Numpy array with shape {any.shape} and dtype {any.dtype}'
+        elif torch is not None and isinstance(any, torch.Tensor):
+            return f'Torch Tensor with shape {any.shape} and dtype {any.dtype}'
+        elif isinstance(any, set):
+            return str(any)
+        elif isinstance(any, (bytes, bytearray)):
+            return f'Bytes/Bytearray of length {len(any)}'
+        elif hasattr(any, '__dict__'):
+            return f'Object of type {type(any).__name__} with attributes {any.__dict__}'
+        else:
+            return f'Object of type {type(any).__name__}'
--- web/assets/js/DisplayInfo_v2.js
+++ web/assets/js/DisplayInfo_v2.js
@@ -1,58 +1,35 @@
 import { app } from "../../scripts/app.js";
 import { ComfyWidgets } from "../../scripts/widgets.js";
 
-// Displays input text on a node
 app.registerExtension({
-	name: "ComfyUI.MaraScott.DisplayInfo_v2",
-	async beforeRegisterNodeDef(nodeType, nodeData, app) {
+    name: "ComfyUI.MaraScott.DisplayInfo_v2",
+    async beforeRegisterNodeDef(nodeType, nodeData, app) {
+        if (!nodeData?.category?.startsWith("MaraScott")) {
+            return;
+        }
+
         if (nodeData.name === "MaraScottDisplayInfo_v2") {
-			function populate(text) {
-				if (this.widgets) {
+            const onExecuted = nodeType.prototype.onExecuted;
+
+            nodeType.prototype.onExecuted = function (message) {
+                onExecuted?.apply(this, arguments);
+
+                if (this.widgets) {
 					for (let i = 1; i < this.widgets.length; i++) {
 						this.widgets[i].onRemove?.();
 					}
-					this.widgets.length = 0;
+					this.widgets.length = 1;
 				}
 
-				const v = [...text];
-				if (!v[0]) {
-					v.shift();
-				}
-				for (const list of v) {
-                    
-					const w = ComfyWidgets["STRING"](this, "text", ["STRING", { multiline: true }], app).widget;
-					w.inputEl.readOnly = true;
-					w.inputEl.style.opacity = 0.6;
-					w.value = list;
-				}
-
-				requestAnimationFrame(() => {
-					const sz = this.computeSize();
-					if (sz[0] < this.size[0]) {
-						sz[0] = this.size[0];
-					}
-					if (sz[1] < this.size[1]) {
-						sz[1] = this.size[1];
-					}
-					this.onResize?.(sz);
-					app.graph.setDirtyCanvas(true, false);
-				});
-			}
-
-			// When the node is executed we will be sent the input text, display this in the widget
-			const onExecuted = nodeType.prototype.onExecuted;
-			nodeType.prototype.onExecuted = function (message) {
-				onExecuted?.apply(this, arguments);
-				populate.call(this, message.text);
-			};
-
-			const onConfigure = nodeType.prototype.onConfigure;
-			nodeType.prototype.onConfigure = function () {
-				onConfigure?.apply(this, arguments);
-				if (this.widgets_values?.length) {
-					populate.call(this, this.widgets_values);
-				}
-			};
-		}
-	},
+                // Check if the "text" widget already exists.
+                let textWidget = this.widgets && this.widgets.find(w => w.name === "text");
+                if (!textWidget) {
+                    textWidget = ComfyWidgets["STRING"](this, "text", ["STRING", { multiline: true }], app).widget;
+                    textWidget.inputEl.readOnly = true;
+                    textWidget.inputEl.style.opacity = 0.6;
+                }
+                textWidget.value = message["text"].join('');
+            };
+        }
+    },
 });
