61d56d359a9c83deb23d7b950697e3528ed36fb3
async () = process.env.GIT_USERNAME || (await ghUser()).email && (await ghUser()).name || DIE("Missing env.GIT_USERNAME") <async () = process.env.GIT_USEREMAIL || (await ghUser()).email && (await ghUser()).email || DIE("Missing env.GIT_USEREMAIL")>
2025-11-27 01:36:18 +0000

    chore(pyproject): Add pyproject.toml for Custom Node Registry

--- py/utils/version.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-#
-###
-
-VERSION = "5.8.9"
--- web_src/src/js/AnyBus_v2.js
+++
@@ -1,5 +0,0 @@
-import { app } from "../../scripts/app.js";
-import { MaraScottAnyBusNodeExtension, MaraScottAnyBusNodeSidebarTab } from "./nodes/AnyBus_v2.js";
-
-app.registerExtension(MaraScottAnyBusNodeExtension());
-app.extensionManager.registerSidebarTab(MaraScottAnyBusNodeSidebarTab());
